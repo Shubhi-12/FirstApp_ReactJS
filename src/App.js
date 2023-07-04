460fbd572e234b0972902972504d8314af819a35
Shubhi Shukla <shubhishukla7309@gmail.com>
2023-07-04 12:10:00 +0530

    enabled dark mode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [mode , setMode] = useState('light');
+
+  const toggleMode=()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#343a40';
+
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-        <Navbar title="PlaywithText" about ="About" />
-        <div className="container my-3"><Textform heading="Enter your text below in the box ..."/></div>
+        
+        <Navbar title="PlaywithText" mode ={mode} toggleMode={toggleMode}/>
+        <div className="container my-3">
+          <Textform heading="Enter your text below in the box ..." mode={mode}/>
+        </div>
     </> );
 }
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -14,6 +14,18 @@ export default function Textform(props) {
     let newtext = '';
     setText(newtext);
   }
+  let HandleCopy=()=>{
+    var text = document.getElementById("mytext");
+    text.select()
+    navigator.clipboard.writeText(text.value);
+    
+  }
+  let HandleExtraSpaces=()=>{
+    let newtext = text.split(/[ ]+/);
+    setText(newtext.join(" "))
+  }
+
+
 
   let HandleChange=(event)=>{
     setText(event.target.value)
@@ -21,23 +33,26 @@ export default function Textform(props) {
   const [text, setText] = useState('');
   return (
     <>
-    <div className="container">
-     
-<div className="mb-3">
-    <h2>{props.heading}</h2>
-  
-    <textarea className="form-control" value={text} onChange={HandleChange} id="mytext" rows="8"></textarea>
-</div>
-<button className="btn btn-success mx-2" onClick={HandleUClick}>Convert into UpperCase</button>
-<button className="btn btn-success mx-2" onClick={HandleLClick}>Convert into LowerCase</button>
-<button className="btn btn-success mx-2" onClick={HandleClear}>Clear the Text</button>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+        <div className="mb-3">
+            <h2>{props.heading}</h2>
+            <textarea className="form-control" value={text} onChange={HandleChange} id="mytext" rows="8"style={{backgroundColor:props.mode==='dark'?'grey':'white' , color:props.mode==='dark'?'white':'black'}}></textarea>
+        </div>
+        <button className="btn btn-success mx-2" onClick={HandleUClick}>Convert into UpperCase</button>
+        <button className="btn btn-success mx-2" onClick={HandleLClick}>Convert into LowerCase</button>
+        <button className="btn btn-success mx-2" onClick={HandleClear}>Clear the Text</button>
+        <button className="btn btn-success mx-2" onClick={HandleCopy}>Copy the Text</button>
+        <button className="btn btn-success mx-2" onClick={HandleExtraSpaces}>Remove Extra Spaces</button>
     </div>
-    <div className="container">
-      <h2>Summary of your Text:</h2>
-      <p>{text.split(" ").length} Words and {text.length} Characters in the text</p>
+
+
+    <div className="container" style={{color:props.mode ==='dark'?'white':'black'}}>
+        <h2>Summary of your Text:</h2>
+        <p>{text.split(" ").length-1} Words and {text.length} Characters in the text</p>
+        <h2>  Preview:</h2>
+        <p>{text}</p>
     </div>
-    <h2>Preview:</h2>
-    <p>{text}</p>
+    
     </>
   )
 }
